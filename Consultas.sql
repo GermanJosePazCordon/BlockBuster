924b0563e48239b9814ff58b65fca28d5deaa60c
GermanJosePazCordon <2776871601601@ingenieria.usac.edu.gt>
2021-09-29 00:39:01 -0600

    consulta 9, 10

--- Consultas.sql
+++ Consultas.sql
@@ -4,26 +4,25 @@ select sum(cantidad) as numero_copias from
 from Stock
 inner join Tienda on Tienda.id_tienda = Stock.id_tienda
 inner join Pelicula on Pelicula.id_pelicula = Stock.id_pelicula
-where Pelicula.titulo = 'SUGAR WONKA'
-) foo;
+where upper(Pelicula.titulo) = 'SUGAR WONKA')foo;
 
 ------------------------------ CONSULTA 2 -----------------------------------
-select Cliente.nombre, Cliente.apellido, SUM(Alquiler.monto) as monto
+select Cliente.nombre, Cliente.apellido, trunc(sum(Alquiler.monto)::decimal,2) as monto
 from Alquiler
 inner join Cliente on Cliente.id_cliente = Alquiler.id_cliente
-Group by Cliente.nombre, Cliente.apellido having count(*) >= 40;
+Group by Cliente.nombre, Cliente.apellido having count(*) >= 40 order by Cliente.nombre asc;
 
 ------------------------------ CONSULTA 3 -----------------------------------
 select concat(nombre,' ', apellido) as Nombre_completo
 from Actor
-where upper(apellido) like '%SON%' order by nombre ASC;
+where upper(apellido) like '%SON%' order by nombre asc;
 
 ------------------------------ CONSULTA 4 -----------------------------------
 select distinct nombre, apellido, lanzamiento
 from Reparto
 inner join Actor ON Reparto.id_actor = Actor.id_actor
 inner join Pelicula ON Reparto.id_pelicula = Pelicula.id_pelicula
-where upper(descripcion) like '%CROCODILE%' and upper(descripcion) like '%SHARK%' order by apellido ASC;
+where upper(descripcion) like '%CROCODILE%' and upper(descripcion) like '%SHARK%' order by apellido asc;
 
 ------------------------------ CONSULTA 5 -----------------------------------
 with rentador as (select Cliente.nombre, Cliente.apellido,
@@ -92,3 +91,42 @@ from rentas_pais, rentas_sport, Pais
 where rentas_pais.pais = Pais.pais and rentas_sport.pais = pais.pais
 group by rentas_pais.pais, rentas_sport.pais, rentas_pais.count, rentas_sport.count 
 order by rentas_pais.pais asc;
+
+------------------------------ CONSULTA 9 -----------------------------------
+with city as (select Ciudad.nombre, count(*) 
+from Alquiler, Cliente, Ciudad, Pais 
+where upper(Ciudad.nombre) <> 'DAYTON' and upper(Pais.pais) = 'UNITED STATES' 
+and Alquiler.id_cliente = Cliente.id_cliente and Ciudad.id_direccion = Cliente.id_direccion
+and Ciudad.id_pais = Pais.id_pais group by Ciudad.nombre),
+daytons as (select count(*) 
+from Alquiler, Cliente, Ciudad, Pais 
+where upper(Ciudad.nombre) = 'DAYTON' and upper(Pais.pais) = 'UNITED STATES'
+and Alquiler.id_cliente = Cliente.id_cliente and Ciudad.id_direccion = Cliente.id_direccion
+and Ciudad.id_pais = Pais.id_pais)
+select city.nombre as Ciudad, city.count as renta
+from city, daytons
+group by city.nombre,city.count, daytons.count having city.count>daytons.count
+order by Ciudad asc;
+
+------------------------------ CONSULTA 10 -----------------------------------
+with rentas_horror as (select Pais.pais, Ciudad.nombre as Ciudad, count(*)
+from Pais, Alquiler, Ciudad, Cliente, Pelicula, Categoria, Genero
+where Cliente.id_cliente = Alquiler.id_cliente and Cliente.id_direccion = Ciudad.id_direccion 
+and Pais.id_pais = Ciudad.id_pais and Alquiler.id_pelicula = Pelicula.id_pelicula
+and Genero.id_pelicula = Pelicula.id_pelicula and Genero.id_categoria = Categoria.id_categoria
+and upper(categoria.categoria) = 'HORROR'
+group by Pais.id_pais, Ciudad),
+rentas_otros as (select Pais.pais, Ciudad.nombre as Ciudad, count(*)
+from Pais, Alquiler, Ciudad, Cliente, Pelicula, Categoria, Genero
+where Cliente.id_cliente = Alquiler.id_cliente and Cliente.id_direccion = Ciudad.id_direccion 
+and Pais.id_pais = Ciudad.id_pais and Alquiler.id_pelicula = Pelicula.id_pelicula
+and Genero.id_pelicula = Pelicula.id_pelicula and Genero.id_categoria = Categoria.id_categoria
+and upper(categoria.categoria) <> 'HORROR'
+group by Pais.id_pais, Ciudad, Categoria.categoria),
+comparador as (select rentas_otros.ciudad, max(rentas_otros.count) 
+from rentas_otros
+group rentas_otros.ciudad)
+select rentas_horror.pais, rentas_horror.ciudad
+from comparador, rentas_horror
+where comparador.ciudad = rentas_horror.ciudad and rentas_horror.count >= comparador.max 
+order by rentas_horror.pais asc;
