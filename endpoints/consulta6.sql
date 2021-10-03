65c649bf0d5088799ab2a6be4b8f10fde0b828d1
GermanJosePazCordon <2776871601601@ingenieria.usac.edu.gt>
2021-10-03 00:23:26 -0600

    final

--- endpoints/consulta6.sql
+++ endpoints/consulta6.sql
@@ -1,17 +1,14 @@
-with residente as (select Pais.pais, Ciudad.nombre, Cliente.nombre
-from Pais, Cliente, Ciudad
+with conteo_pais as (select Pais.id_pais,Pais.pais, count(*)::decimal  
+from  Pais, Ciudad, Cliente
+where Pais.id_pais = Ciudad.id_pais and Cliente.id_direccion = Ciudad.id_direccion
+group by Pais.id_pais, Pais.pais),
+conteo_ciudad as (select Ciudad.id_pais, Ciudad.nombre, count(*)::decimal
+from Ciudad, Cliente,  Pais
 where Cliente.id_direccion = Ciudad.id_direccion
-and Pais.id_pais = Ciudad.id_pais),
-clientes_pais as (select Pais.pais, count(*)::decimal 
-from residente, Pais
-where Pais.pais = residente.pais 
-group by Pais.pais),
-clientes_ciudad as (select Pais.pais,Ciudad.nombre, count(*)
-from Ciudad, Cliente, Pais
-where Cliente.id_direccion = Ciudad.id_direccion and Ciudad.id_pais = Pais.id_pais
-group by Ciudad.nombre, Cliente.id_direccion, Pais.pais order by Pais.pais asc)
-select clientes_ciudad.pais, clientes_pais.count as cliente_pais, clientes_ciudad.nombre as Ciudad, 
-clientes_ciudad.count as cliente_ciudad,
-concat(trunc((clientes_ciudad.count/clientes_pais.count)*100,4),'%') as Porcentaje
-from clientes_pais, clientes_ciudad
-where clientes_pais.pais = clientes_ciudad.pais;
+and Pais.id_pais = Ciudad.id_pais
+group by Ciudad.id_pais, Ciudad.nombre
+order by Ciudad.id_pais, Ciudad.nombre asc)
+select conteo_pais.pais, conteo_pais.count as cliente_pais, conteo_ciudad.nombre, conteo_ciudad.count as cliente_ciudad, 
+concat(trunc((conteo_ciudad.count/conteo_pais.count)*100,4),'%') as Porcentaje
+from conteo_pais, conteo_ciudad 
+where conteo_pais.id_pais = conteo_ciudad.id_pais;
